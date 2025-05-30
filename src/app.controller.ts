a98394e31b6ebfb9482c9dd4ef7270efb3a08dba
Olumide <olumidebamgboje@gmail.com>
2025-05-30 23:53:24 +0100

    added a get endpoint to the app.contoller for google ads verification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Controller()
 export class AppController {
@@ -8,5 +11,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('ads.txt')
+  getAdsTxt(@Res() res: Response) {
+    const adsFilePath = path.join(process.cwd(), 'Ads.txt');
+    if (fs.existsSync(adsFilePath)) {
+      const content = fs.readFileSync(adsFilePath, 'utf8');
+      res.type('text/plain').send(content);
+    } else {
+      res.status(404).send('Ads.txt not found');
+    }
   }
 }
